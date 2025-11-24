33b0e773effdf288de9edf60292e6a68a3b944fd
faelinshade <fadhil258729@student.smkn1kandeman.sch.id>
2025-11-24 07:45:01 +0700

    Add files via upload

---
+++ Praktik 5.2/Praktik 5.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            Console.WriteLine("Menghitung jumlah angka dari 1");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                total += i;
+
+                Console.WriteLine("Menambahkan" + i + ", total sementara=" + total);
+            }
+            Console.WriteLine("\n Hasil akhir  penjumlahan adalah: " + total);
+        }
+    }
+}
