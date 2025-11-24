b409ea571b3b2f897cfbff2805402c7ea6b94f0d
faelinshade <fadhil258729@student.smkn1kandeman.sch.id>
2025-11-24 07:47:31 +0700

    Add files via upload

---
+++ Praktik 7.2/Praktik 7.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_7._2
+{
+    internal class Program
+    {
+        static void SapaNama(string nama)
+        {
+            Console.WriteLine("Halo, " + nama + "!");
+            Console.WriteLine("semangat belajar C# hari ini!");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan nama Anda: ");
+            string namaSiswa = Console.ReadLine();
+            SapaNama(namaSiswa);
+        }
+    }
+}
