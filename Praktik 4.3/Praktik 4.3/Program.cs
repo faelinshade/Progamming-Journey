143bc3ca1ac20d0a79a0405389ca3a9c6165b37b
faelinshade <fadhil258729@student.smkn1kandeman.sch.id>
2025-11-24 07:32:06 +0700

    Add files via upload

---
+++ Praktik 4.3/Praktik 4.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik_4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan nilai ujian: ");
+            int nilai = int.Parse(Console.ReadLine());
+
+            if (nilai >= 75)
+            {
+                Console.WriteLine("Selamat, Anda Lulus!");
+            }
+        }
+    }
+}
